94a8fc4465950a1b7ac648ee5e1e11e0fe9cf68c
xiaochun <wuchunrong2014@gmail.com>
2015-01-19 12:25:08 +0800

    Update Action.java

--- src/pql/action/Action.java
+++ src/pql/action/Action.java
@@ -2,7 +2,51 @@ package pql.action;
 
 import java.awt.Graphics;
 
+/**
+
+* 把所有要处理的事件抽象为一个Action借口
+
+* @author chunlan
+
+* @Time 2015-1-18 10:30:00
+
+*/
+
 public interface Action {
+	
+	/**
+
+	 * 该方法用于处理所有要完成的一系列的动作
+
+	 *@param color按钮的颜色
+
+	 *@return void
+
+	 *@exception  
+
+	 *@author chunlan
+
+	 *@Time 2015-1-18 10:30:00
+
+	 */
+	 
 	public void doAction();
+	
+	/**
+
+	 * 该方法用于在面板中绘出方法内的所有实体
+
+	 *@param g 画笔
+
+	 *@return void
+
+	 *@exception  
+
+	 *@author chunlan
+
+	 *@Time 2015-1-18 10:30:00
+	 
+	 */
+	 
 	public void drawAction(Graphics g);
 }
