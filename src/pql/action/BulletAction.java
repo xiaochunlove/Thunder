e3e711c15a2b12f5ed2b36dd539cbdf36e832e46
xiaochun <wuchunrong2014@gmail.com>
2015-01-19 13:42:56 +0800

    Update BulletAction.java

--- src/pql/action/BulletAction.java
+++ src/pql/action/BulletAction.java
@@ -14,14 +14,25 @@ import pql.model.HeroBullet_3;
 import pql.model.IBullet;
 import pql.model.Point;
 
+/**
+ * 子弹处理类实现Action借口
+ * 
+ * @author chunlan
+ * @Time 2015-1-18 10:30:00
+ */
 public class BulletAction implements Action{
 	public MainPanel mainPanel = null;
 	private PlaneAction planeAction = null;
-	public List<Point> heroBullets = new ArrayList<Point>();
-	public List<Point> enermyBullets = new ArrayList<Point>();
+	public List<Point> heroBullets = new ArrayList<Point>();			//英雄机子弹集
+	public List<Point> enermyBullets = new ArrayList<Point>();			//敌机子弹集
 	public IBullet heroBullet = null;
 	public IBullet enermyBullet = null;
 	
+	/**
+	 * 子弹处理类的构造函数，初始化飞机处理类，子弹处理类和分数处理类以及子弹宽度
+	 *  
+	 * @param m 面板
+	 */
 	public BulletAction(MainPanel m) {
 		this.mainPanel = m;
 		this.heroBullet = new HeroBullet_1(this);
@@ -36,6 +47,14 @@ public class BulletAction implements Action{
 		});
 	}
 	
+	/**
+	 * 该方法内通过调用英雄机子弹的路径方法以及敌机子弹的添加和路径方法来绘制子弹的运动轨迹
+	 * 
+	 * @return void
+	 * @exception  
+	 * @author chunlan
+	 * @Time 2015-1-18 10:30:00
+	 */
 	@Override
 	public void doAction() {
 		heroBullet.route();
@@ -43,12 +62,30 @@ public class BulletAction implements Action{
 		enermyBullet.route();
 	}
 	
+	/**
+	 * 该方法内通过调用英雄机子弹和敌机子弹的绘图方法来绘制子弹
+	 * 
+	 * @param g 画笔
+	 * @return void
+	 * @exception  
+	 * @author chunlan
+	 * @Time 2015-1-18 10:30:00
+	 */
 	@Override
 	public void drawAction(Graphics g) {
 		heroBullet.draw(g);
 		enermyBullet.draw(g);
 	}
 	
+	/**
+	 * 该方法设置子弹的类型
+	 * 
+	 * @param type 类型
+	 * @return void
+	 * @exception  
+	 * @author chunlan
+	 * @Time 2015-1-18 10:30:00
+	 */
 	public void setHeroBullet(int type) {
 		if(type == 1) {
 			this.heroBullet = new HeroBullet_1(this);
