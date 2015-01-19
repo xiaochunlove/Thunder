0d227610fa4c1cd4099f7fb118af3b2ba4b3dcb6
xiaochun <wuchunrong2014@gmail.com>
2015-01-19 13:24:40 +0800

    Update BulletAction.java

--- src/pql/action/BulletAction.java
+++ src/pql/action/BulletAction.java
@@ -12,14 +12,27 @@ import pql.model.HeroBullet_1;
 import pql.model.IBullet;
 import pql.model.Point;
 
+/**
+ * 子弹处理类实现Action借口
+ * 
+ * @author chunlan
+ * @Time 2015-1-18 10:30:00
+ */
+ 
 public class BulletAction implements Action{
 	public MainPanel mainPanel = null;
 	private PlaneAction planeAction = null;
-	public List<Point> heroBullets = new ArrayList<Point>();
-	public List<Point> enermyBullets = new ArrayList<Point>();
-	public IBullet heroBullet = null;
+	public List<Point> heroBullets = new ArrayList<Point>();			//英雄机子弹集
+	public List<Point> enermyBullets = new ArrayList<Point>();			//敌机子弹集
+	public IBullet heroBullet = null;						
 	public IBullet enermyBullet = null;
 	
+	/**
+	 * 子弹处理类的构造函数，初始化飞机处理类，子弹处理类和分数处理类以及子弹宽度
+	 *  
+	 * @param m 面板
+	 */
+	 
 	public BulletAction(MainPanel m) {
 		this.mainPanel = m;
 		this.heroBullet = new HeroBullet_1(this);
